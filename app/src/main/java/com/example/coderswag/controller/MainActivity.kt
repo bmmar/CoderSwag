e9aad3b92bf02370325fea5cf62b5f00432613b2
Brian Marshall <bm.marshall@btinternet.com>
2021-07-09 07:10:40 +0100

    Lesson 01

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.coderswag
+package com.example.coderswag.controller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.coderswag.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
